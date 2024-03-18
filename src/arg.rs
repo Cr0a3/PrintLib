543fa112b09fe7335253052ec35f80674ebcd8a3
Cr0a3 <127748753+Toni-Graphics@users.noreply.github.com>
2024-03-18 14:34:24 +0100

    Update arg.rs

--- src/arg.rs
+++ src/arg.rs
@@ -14,32 +14,6 @@ struct Opt {
     does: String,
 }
 
-fn align(mut s1: String, len: usize) -> String {
-    let len1 = s1.len();
-    
-    if len1 < len {
-        s1.push_str(&" ".repeat(len - len1));
-    }
-    
-    s1
-}
-
-fn longest_string(strings: &Vec<Opt>) -> Option<String> {
-    if strings.is_empty() {
-        return None;
-    }
-
-    let mut longest = &strings[0];
-
-    for s in strings.iter() {
-        if s.name.len() > longest.name.len() {
-            longest = s;
-        }
-    }
-
-    Some(longest.name.clone())
-}
-
 impl UsageFactory {
     pub fn new(name: &str, app_name: &str) -> Self {
         Self {
@@ -59,23 +33,6 @@ impl UsageFactory {
     }
 
     pub fn print(&mut self) {
-        // aling
-        let long_cmd = longest_string(&self.cmds).expect("Error while geting longest").len();
-        let long_opt = longest_string(&self.options).expect("Error while geting longest").len();
-        let longest = max(long_cmd, long_opt);
-
-        let mut i = 1;
-        while i < self.cmds.len() {
-            self.cmds[i].name = align(self.cmds[i].name.clone(), longest);
-            i += 1;
-        }
-
-        let mut i = 1;
-        while i < self.options.len() {
-            self.cmds[i].name = align(self.cmds[i].name.clone(), longest);
-            i += 1;
-        }
-
         //print
         println!("{}", self.name.color(0, 42, 71).bold());
         println!();
